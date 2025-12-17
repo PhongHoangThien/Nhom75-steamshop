d6bc6ce9949048a8bd5ac11e39de7e7a5ab5f5f5
PhongHoangThien <22130264@st.hcmuaf.edu.vn>
2025-12-17 22:07:07 +0700

    Config app route and add some route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-// import './css/app.css';
 import NavBar from "./components/NavBar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HonePage";
-import GameDetailPage from "./pages/ProductDetails";
+import GameDetailPage from "./pages/GameDetails";
 import Footer from "./components/Footer";
+import Games from "./pages/Games";
 
 function App() {
   return (
@@ -12,7 +12,15 @@ function App() {
           <NavBar />
           <Routes>
               <Route path="/" element={<HomePage />} />
+              <Route path="/games" element={<Games />} />
               <Route path="/game/:id" element={<GameDetailPage />} />
+              {/*<Route path="/best-seller" element={<BestSeller />} />*/}
+              {/*<Route path="/game-on-sale" element={<GameOnSale />} />*/}
+              {/*<Route path="/cart" element={<Cart />} />*/}
+              {/*<Route path="/login" element={<Login />} />*/}
+              {/*<Route path="/register" element={<Register />} />*/}
+              {/*<Route path="/user-profile" element={<UserProfile />} />*/}
+              {/*<Route path="/checkout" element={<Checkout />} />*/}
           </Routes>
           <Footer />
       </BrowserRouter>
